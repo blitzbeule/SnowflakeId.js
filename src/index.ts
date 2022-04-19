33439e033d9f7225c5f4b92ef347b7d15ea0e4a8
blitzbeule <38007250+blitzbeule@users.noreply.github.com>
2022-04-19 18:22:34 +0200

    updated exports

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { SnowflakeGenerator } from "./generator";
 
+export { SnowflakeGenerator } from "./generator";
 export { Structure as SnowflakeStructure } from "./structure";
 export { TimeSource } from "./time/time_source";
 export { SimpleTimeSource } from "./time/simple_time_source";
